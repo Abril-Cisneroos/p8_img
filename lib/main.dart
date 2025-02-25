63060f6dd0dcf1140dd09dcdb4c89638f78ab06b
FRIDA_ABRIL_CISNEROS_HERNANDEZ <a.22308051281162@cbtis128.edu.mx>
2025-02-25 17:01:06 +0000

    actividad 8

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,53 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MiImagen());
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
+class MiImagen extends StatelessWidget {
+  const MiImagen({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text("Mis imagenes"),
+          backgroundColor: Colors.cyan,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text(
+                "Frida Abril cisneros Hernandez."
+                " Mat:22308051281162",
+                style: TextStyle(fontSize: 16, color: Colors.brown),
+              ),
+              Image.network(
+                'https://raw.githubusercontent.com/Abril-Cisneroos/im-genes-para-App-flutter-6J/refs/heads/main/perro1.jpg', // Reemplaza con la URL de tu primera imagen
+                height: 200,
+                width: 200, // Ajusta la altura según tus necesidades
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Perro en cosulta ",
+                style: TextStyle(fontSize: 16, color: Colors.brown),
+              ), // Espacio entre las imágenes
+              Image.network(
+                'https://raw.githubusercontent.com/Abril-Cisneroos/im-genes-para-App-flutter-6J/refs/heads/main/perro2.jpg', // Reemplaza con la URL de tu segunda imagen
+                height: 200,
+                width: 200, // Ajusta la altura según tus necesidades
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Perro con vetrinaria ",
+                style: TextStyle(fontSize: 16, color: Colors.brown),
+              ),
+            ],
+          ),
         ),
       ),
     );
-  }
-}
+    //material app
+  } //widgets
+} //Clase MiImagen
